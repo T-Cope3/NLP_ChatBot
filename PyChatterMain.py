1b77f018b4e027c71d8385a78ca2922816d12dcb
Troy Cope <troycope1337@gmail.com>
2022-11-20 15:23:33 -0500

    Fixed some code, added default to bot, more comments

--- PyChatterMain.py
+++ PyChatterMain.py
@@ -1,6 +1,6 @@
 import nltk, json, random, pickle
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
+lemat = WordNetLemmatizer()
 import pickle
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -8,65 +8,73 @@ model = load_model('foodbotModel.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
+engageDefault = True
 
-#Need to comment and change this file a lot
-#Hookup to frontend UI
-#Make process for food req -> new trained model maybeeeeee?
+# Need to comment and change this file a lot
+# Hookup to frontend UI
+# Make process for food req -> new trained model maybeeeeee?
 # Tons more comments can be here and more intuitive names
 
 # preprocess user input
 # tokenizing and lamenting for less processing
 def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    return sentence_words
+    wordsInSentence = nltk.word_tokenize(sentence)
+    shortenedWords = [lemat.lemmatize(word.lower()) for word in wordsInSentence]
+    return shortenedWords
 
 # create bag of words
 # just like text vector array shown in class
 def bagOfWords(sentence, words, onPrint=True):
-    sentence_words = clean_up_sentence(sentence)
+    shortenedWords = clean_up_sentence(sentence)
     bag = [0]*len(words)
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s:
-                bag[i] = 1
+    for sents in shortenedWords:
+        for index,word in enumerate(words):
+            if word == sents:
+                bag[index] = 1
+                global engageDefault
+                engageDefault = False
                 if onPrint:
-                    print ("found in bag: %s" % w)
+                    print(f"Word present in bag: {word}")
     return(np.array(bag))
 
-def calc_pred(sentence, model):
+def makePrediction(sentence, model):
     p = bagOfWords(sentence, words, onPrint=False)
     res = model.predict(np.array([p]), verbose=0)[0]
 
-    print(f"res: {res} from {classes}")
+    # print(f"res: {res} from {classes}")
     # Can adjust error as necessary, most common might be .6 vs .4 or less competitive.
     # .25 is fine for now but can be heightend for more certainty
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
+    REMOVE_THRESHOLD = 0.25
     return_list = []
-    for r in results:
-        # Spitting back the most likely w/ probability
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    global engageDefault
+
+    if not engageDefault:
+        results = [[i,r] for i,r in enumerate(res) if r>REMOVE_THRESHOLD]
+        # sort by strength of probability
+        results.sort(key=lambda x: x[1], reverse=True)
+        for r in results:
+            # Spitting back the most likely w/ probability
+            return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    else:
+        return_list.append({"intent": "default", "probability": str(1)})
+    engageDefault = True
     return return_list
 
-def getResponse(ints, intents_json):
+def getResponse(pred, jsonIntent):
     # currently defaulting to whatever model predicts most highly
     # 
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag'] == tag):
-            result = random.choice(i['responses'])
+    tag = pred[0]['intent']
+    intentList = jsonIntent['intents']
+    for intent in intentList:
+        if(intent['tag'] == tag):
+            result = random.choice(intent['responses'])
             break
     return result
 
-def send(msg):
-    ints = calc_pred(msg, model)
-    print(f"Calculated Data from calc_pred: {ints}")
-    res = getResponse(ints, intents)
+def messageFromAI(userMessage):
+    pred = makePrediction(userMessage, model)
+    # print(f"Calculated Data from calc_pred: {ints}")
+    res = getResponse(pred, intents)
     return res
 
 # user = ''
@@ -82,12 +90,11 @@ def send(msg):
 
 
 import PySimpleGUI as sg
-# 需要導入模塊: import PySimpleGUI [as 別名]
-# 或者: from PySimpleGUI import Multiline [as 別名]
+
 def ChatBotWithHistory():
-    mainOutputWidth = 160;
-    # -------  Make a new Window  ------- #
-    sg.ChangeLookAndFeel('GreenTan')            # give our form a spiffy set of colors
+    mainOutputWidth = 160
+    #New windows with style
+    sg.ChangeLookAndFeel('GreenTan')
 
     #Need to use monospace font in order to ensure the windowSize claculation remains true
     layout =  [[sg.Text('Your output will go here', size=(40, 1))],
@@ -98,9 +105,8 @@ def ChatBotWithHistory():
                 sg.Button('SEND', size=(10,5), border_width=5, button_color=(sg.YELLOWS[0], sg.BLUES[0]), bind_return_key=True),
                 sg.Button('EXIT', size=(10,5), border_width=5, button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
 
-    window = sg.Window('ChatBot Simplification', default_element_size=(30, 2), font=('Helvetica',' 12'), default_button_element_size=(8,2), return_keyboard_events=True).Layout(layout)
-    #---===---Loop taking in user input and using it---#
-    userFlag = True
+    window = sg.Window('FoodBot Chatterface', default_element_size=(30, 2), font=('Helvetica',' 12'), default_button_element_size=(8,2), return_keyboard_events=True).Layout(layout)
+    #Snagging user input and spitting back
     command_history = []
     history_offset = 0
     while True:
@@ -108,33 +114,34 @@ def ChatBotWithHistory():
         if event == 'SEND':
             query = value['query'].rstrip()
             # EXECUTE YOUR COMMAND HERE
-            if(userFlag):
-                print(("USER: {}".format(query)).rjust(mainOutputWidth))
-                command_history.append(query)
-                history_offset = len(command_history)-1
-                window.find_element('query').Update('')                 # manually clear input because keyboard events blocks clear
-                window.find_element('history').Update('\n'.join(command_history[-3:]))
+            print(("USER: {}".format(query)).rjust(mainOutputWidth))
+            command_history.append(query)
+            history_offset = len(command_history)-1
+            window.find_element('query').Update('')
+            # manually clear input because keyboard events blocks clear
+            window.find_element('history').Update('\n'.join(command_history[-3:]))
 
-                userInput = query
-                res = send(userInput)
-                print("BOT: {}".format(res))
-                window.find_element('query').Update('')
+            userInput = query
+            response = messageFromAI(userInput)
+            print("BOT: {}".format(response))
+            window.find_element('query').Update('')
                 
-        elif event in (None, 'EXIT'):            # quit if exit event or X
+        #Quits if X clicked or Exit is pressed
+        elif event in (None, 'EXIT'):
             break
         elif 'Up' in event and len(command_history):
             command = command_history[history_offset]
-            history_offset -= 1 * (history_offset > 0)      # decrement is not zero
+            # decrement is not zero
+            history_offset -= 1 * (history_offset > 0)
             window.find_element('query').Update(command)
         elif 'Down' in event and len(command_history):
-            history_offset += 1 * (history_offset < len(command_history)-1) # increment up to end of list
+            # increment up to end of list
+            history_offset += 1 * (history_offset < len(command_history)-1) 
             command = command_history[history_offset]
             window.find_element('query').Update(command)
         elif 'Escape' in event:
             window.find_element('query').Update('')
     window.close()
 
-
-
-#rjust, pyton padding, for total characters
+# Calls UI with the model implemented
 ChatBotWithHistory() 
--- modelCreation.py
+++ modelCreation.py
@@ -1,7 +1,7 @@
 import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer
 
 import json
@@ -14,8 +14,7 @@ from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
 
 # Tons more comments can be here and more intuitive names
-
-lemmatizer = WordNetLemmatizer()
+lemat = WordNetLemmatizer()
 intentFile = open('intents.json').read()
 jsonIntent = json.loads(intentFile)
 
@@ -38,8 +37,8 @@ for intentDict in jsonIntent['intents']:
             classes.append(intentDict['tag'])
 
 
-itemsToIgnore = ['?', '!']
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in itemsToIgnore]
+itemsToIgnore = ['?', '!', '.', ',', '\'']
+words = [lemat.lemmatize(word.lower()) for word in words if word not in itemsToIgnore]
 
 pickle.dump(words, open('words.pkl','wb'))
 pickle.dump(classes, open('classes.pkl','wb'))
@@ -49,8 +48,8 @@ print(f"Docs: {documents}")
 
 
 
-#THESE VARS NEEEEED TO BE HERE SO THEY ARE RESET / PERSISTENT PROPERLY FOR EACH DOC
-training = []
+# THESE VARS NEEEEED TO BE HERE SO THEY ARE RESET / PERSISTENT PROPERLY FOR EACH DOC
+patternAndIntents = []
 output_empty = list([0] * len(classes))
 for doc in documents:
     # bag of words
@@ -58,22 +57,23 @@ for doc in documents:
     # list of tokens
     pattern_words = doc[0]
     # token lemmatization
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    pattern_words = [lemat.lemmatize(word.lower()) for word in pattern_words]
     # if the word matches, I enter 1, otherwise 0
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-    training.append([bag, output_row])
+    output_col = list(output_empty)
+    output_col[classes.index(doc[1])] = 1
+    patternAndIntents.append([bag, output_col])
 
-training = np.array(training, dtype=object)
+training = np.array(patternAndIntents, dtype=object)
 # creation of train and test sets: X - patterns, Y - intents
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 
 # model creation, can be smaller model tbh
-# can make model more efficient too (early stopping?, what else?)
+# can make model more efficient too (what else?)
+# do not need to load weights, model will predict based off saved model not weights
 model = Sequential()
 model.add(Dense(512, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -85,9 +85,9 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-#Standard SGD, usually paired with Cat_Cross, but doesn't matter
-#I have seen this overfit to failure, so using Adam for now
-#sgd = SGD(learning_rate=0.01, weight_decay=1e-6, momentum=0.9, nesterov=True)
+# Standard SGD, usually paired with Cat_Cross, but doesn't matter
+# I have seen this overfit to failure, so using Adam for now
+# sgd = SGD(learning_rate=0.01, weight_decay=1e-6, momentum=0.9, nesterov=True)
 
 # Categorical_cross is specifically for labels and predictions so we are using it
 # Optimizer matters a lot less
@@ -101,21 +101,20 @@ model.compile(
     metrics=['accuracy',tf.keras.metrics.Precision(),tf.keras.metrics.Recall()]
     )
 
-#fitting and saving the model
-#Launch in cmd with TensorFlow pip installed to get the dashboard
-#tensorboard --logdir="C:\Users\Troy Cope\Desktop\PyProject\logs"
+# fitting and saving the model
+# Launch in cmd with TensorFlow pip installed to get the dashboard
+# tensorboard --logdir="C:\Users\Troy Cope\Desktop\PyProject\logs"
 
 # Defining optimal callback items, early stopping will wait until absolutely necessary to cut off weights
 earlyStop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 # Model is training to match the patterns and intents, respectively
 # Still tuning hyper params to feel good, will implement early stopping
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, callbacks=[tensorboard_callback, earlyStop], verbose=1)
-model.save('foodbotModel.h5', hist)
+history = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, callbacks=[tensorboard_callback, earlyStop], verbose=1)
+model.save('foodbotModel.h5', history)
 
 print("All generation is completed!")
 
-# score = model.evaluate(x_test, y_test, verbose = 0) 
-
+# score = model.evaluate(x_test, y_test, verbose = 0)
 # print('Test loss:', score[0]) 
 # print('Test accuracy:', score[1])
